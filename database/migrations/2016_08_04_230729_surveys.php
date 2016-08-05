8b468e019a96e16a89b5fb9888da458e0d558d98
akramabdulrahman <blackthrone91@gmail.com>
2016-08-05 04:34:35 +0300

    migrations done
    all migrations are done , yet there is a problem which prevents me from
    creating foreign keys on service_requests table , the error file is
    located under migrations directory with name : "issues.txt"

---
+++ database/migrations/2016_08_04_230729_surveys.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Surveys extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->timestamp('expires_at');
+            $table->text('description');
+            $table->integer('project_id')->unsigned()->nullable()->index();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('SET NULL');
+
+            $table->integer('survey_meta_id')->unsigned()->index();
+            $table->foreign('survey_meta_id')->references('id')->on('survey_metas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('surveys');
+    }
+
+}
---
+++ database/migrations/2016_08_04_230741_questions.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Questions extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+            $table->integer('survey_id')->unsigned()->nullable();
+            $table->foreign('survey_id')->references('id')->on('surveys')->onDelete('cascade');
+            $table->integer('step')->unsigned();
+            $table->string('rule')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('questions');
+    }
+
+}
